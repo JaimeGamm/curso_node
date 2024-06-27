478f78329dedf495bee155be188a3697d3a71952
jaimegamm <jaimegamm@gmail.com>
2024-06-27 16:56:53 -0500

    clase 3 terminada

--- clase 2/app.js
+++ clase 2/app.js
@@ -1,22 +1,50 @@
-const express = require("express");
+const express = require("express"); // require -> commonJS
 const crypto = require("node:crypto");
+const cors = require("cors");
+
 const movies = require("./movies.json");
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
 
 const app = express();
 app.use(express.json());
-app.disable("x-powered-by");
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:8080",
+        "http://localhost:1234",
+        "https://movies.com",
+        "http://169.254.230.78:8080",
+      ];
 
-app.get("/", (req, res) => {
-  res.json({ message: "hola mundo" });
-});
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
 
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+app.disable("x-powered-by"); // deshabilitar el header X-Powered-By: Express
+
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+// Todos los recursos que sean MOVIES se identifica con /movies
 app.get("/movies", (req, res) => {
   const { genre } = req.query;
   if (genre) {
-    const filtaredMovies = movies.filter((movie) =>
+    const filteredMovies = movies.filter((movie) =>
       movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
     );
-    return res.json(filtaredMovies);
+    return res.json(filteredMovies);
   }
   res.json(movies);
 });
@@ -29,25 +57,65 @@ app.get("/movies/:id", (req, res) => {
 });
 
 app.post("/movies", (req, res) => {
-  const { title, genre, year, director, duration, rate, poster } = req.body;
+  const result = validateMovie(req.body);
 
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  // en base de datos
   const newMovie = {
-    id: crypto.randomUUID(),
-    title,
-    genre,
-    director,
-    year,
-    duration,
-    rate: rate ?? 0,
-    poster,
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data,
   };
 
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
   movies.push(newMovie);
-  res.status(201).json({ newMovie });
+
+  res.status(201).json(newMovie);
+});
+
+app.delete("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  movies.splice(movieIndex, 1);
+
+  return res.json({ message: "Movie deleted" });
+});
+
+app.patch("/movies/:id", (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+
+  movies[movieIndex] = updateMovie;
+
+  return res.json(updateMovie);
 });
 
 const PORT = process.env.PORT ?? 1234;
 
 app.listen(PORT, () => {
-  console.log(`server list on port http://localhost:${PORT}`);
+  console.log(`server listening on port http://localhost:${PORT}`);
 });
